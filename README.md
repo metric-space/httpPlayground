211d9d1ddc8f0360545cd6066d104c1f96d18bb6
luke <nerorevenge@gmail.com>
2015-12-13 15:22:26 -0500

    Response Extractor is working - Got the extract-x-from-response working - Modified readme to show more detail as to how to use the damn thing

--- README.md
+++ README.md
@@ -1,4 +1,23 @@
 ### Goal: place to fool around with http requests
 
-1. Includes a mini combinator(somewhat) library to simplify operations on requests
-2. Just started work on it
+Includes a luke-modified-combinator(somewhat) library to simplify operations on requests
+
+### How to work it
+
+1. open two terminals and navigate to the directory in both
+2. run `activator run` in one to start the local server in one of the terminals @term1
+3. run `activator console` in the other @term2
+4. @term2 put in the following (assuming u=your port is at 9000)
+
+```
+>>> import weirdCombinators._
+
+>>> val url:String = 'http://localhost:9000' 
+
+>>> liftez(url) <*> ("egg","chicken") <||> "GET" </\> "status"
+```
+
+### Important
+
+This mini library uses NingWSClient object and need to be shut down explicitly
+so after you are done call `QUIT` and exit normally(Ctrl+D)
--- app/requestCombinator.scala
+++ app/requestCombinator.scala
@@ -1,7 +1,9 @@
 import play.api.libs.ws.ning._
 import play.api.libs.json._
 import play.api.libs.ws._
-import scala.concurrent.Future
+import scala.concurrent._
+import scala.concurrent.duration._
+import ExecutionContext.Implicits.global
 
 package object weirdCombinators {
 
@@ -20,12 +22,24 @@ package object weirdCombinators {
   }
 
   implicit class addMethodAndExec(x:Option[WSRequest]){
-    // the option is probably useless 
+    // the option is probably useless , needs checking for appropriate method
     def <||> (y:String):Option[Future[WSResponse]] = x.map {req => req.execute(y)}
   }
 
 
-  //implicit class ripOutStatus(x:Option[Future[WSResponse]] ){
-  //  def </\>(y:String):String = "yeah "+y+ " "+x.toString
-  //}
+  implicit class ripOutStatus(x:Option[Future[WSResponse]] ){
+    def </\>(y:String):Any = {
+      // damn this thing is ugly
+      val result = y match {
+        case "status" => x.map { _.onSuccess { case response => response.status}}
+        case "statusText" => x.map { _.onSuccess { case response => response.statusText}}
+        case "body" => x.map { _.onSuccess { case response => response.body}}
+        case _     => "method doesn't exist"
+      }
+      Await.result(x.get,20.seconds) 
+      return result
+    }
+  }
+
+ def QUIT = client.close
 }
